add2586999ce338856662ca4bf2a973128139e7e
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-05-16 23:54:10 +0600

    added new 8kyu kata solution

---
+++ opposites-attract.js
@@ -0,0 +1,39 @@
+/*
+Title: Opposites Attract 
+DESCRIPTION:
+Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+*/
+
+// 1
+function lovefunc(flower1, flower2) {
+  // moment of truth
+  if (
+    (flower1 % 2 === 0 && flower2 % 2 === 1) ||
+    (flower1 % 2 === 1 && flower2 % 2 === 0)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+lovefunc(41, 84);
+lovefunc(62, 44);
+
+// 2
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
+}
+
+lovefunc(1, 4);
+lovefunc(77, 89);
+
+// 3
+function lovefunc(flower1, flower2) {
+  const petals = (flower1 + flower2) % 2 === 1;
+  return petals;
+}
+
+lovefunc(22, 23);
