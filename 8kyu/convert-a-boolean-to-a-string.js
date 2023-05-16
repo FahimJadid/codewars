b6d3656f8dfea79ae0dbe3388af452b04a88ccb2
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-05-17 00:45:52 +0600

    added new 8kyu kata solution

---
+++ 8kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,22 @@
+/*
+TITLE: Convert a Boolean to a String
+DESCRIPTION:
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+// My Solutions
+
+function booleanToString(b) {
+  //your code here
+  return b.toString();
+}
+
+function booleanToString(b) {
+  return b ? 'true' : 'false';
+}
+
+function booleanToString(b) {
+  return b + '';
+}
