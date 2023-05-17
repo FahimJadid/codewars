571f8aa4076552c125ddf0182e8833784de84b1e
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-05-18 03:04:50 +0600

    added new 7kyu kata solution

--- 7kyu/sum-of-two-lowest-positive-integers.js
+++ 7kyu/sum-of-two-lowest-positive-integers.js
@@ -1,5 +1,4 @@
 /*
-
 Title: Sum of two lowest positive integers
 Description:
 Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
