a6905ab7b9d6a0624f2dee53ca640116531273d5
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-05-23 22:13:26 +0600

    added new 7kyu kata solution

--- 7kyu/list-filtering.js
+++ 7kyu/list-filtering.js
@@ -44,3 +44,15 @@ const filter_list = (l) => {
     return acc;
   }, []);
 };
+
+// 5
+
+function filter_list(l) {
+  let filtered = [];
+  for (let i = 0; i < l.length; i++) {
+    if (typeof l[i] === 'number') {
+      filtered.push(l[i]);
+    }
+  }
+  return filtered;
+}
