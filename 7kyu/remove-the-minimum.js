c10526118b39d0acd7ff2df99296c69c3edc6864
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-05-23 23:58:58 +0600

    added new 7kyu kata solution

--- 7kyu/remove-the-minimum.js
+++ 7kyu/remove-the-minimum.js
@@ -25,3 +25,12 @@ function removeSmallest(numbers) {
   const minIndex = numbers.indexOf(Math.min(...numbers));
   return numbers.filter((_, index) => index !== minIndex);
 }
+
+function removeSmallest(numbers) {
+  if (numbers.length === 0) {
+    return [];
+  }
+
+  const minIndex = numbers.indexOf(Math.min(...numbers));
+  return numbers.slice(0, minIndex).concat(numbers.slice(minIndex + 1));
+}
