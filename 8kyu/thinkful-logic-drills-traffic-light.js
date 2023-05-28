2fc2162de037f3375248337b050d1a8464fe9342
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-05-28 23:08:17 +0600

    added new 8kyu kata solution

--- 8kyu/thinkful-logic-drills-traffic-light.js
+++ 8kyu/thinkful-logic-drills-traffic-light.js
@@ -22,3 +22,16 @@ function updateLight(current) {
     return 'invalid';
   }
 }
+
+function updateLight(current) {
+  switch (current) {
+    case 'green':
+      return 'yellow';
+    case 'yellow':
+      return 'red';
+    case 'red':
+      return 'green';
+    default:
+      return 'Invalid state';
+  }
+}
