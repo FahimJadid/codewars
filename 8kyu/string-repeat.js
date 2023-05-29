c6616b4feda6ca5acce852b8c6f8f2b2dc7b05fc
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-05-30 01:10:22 +0600

    added new 8kyu kata solution

--- 8kyu/string-repeat.js
+++ 8kyu/string-repeat.js
@@ -10,14 +10,39 @@ Examples (input -> output)
 
 // Solutions
 
-// PREP
+/*
+Parameter: The function will take in two parameters:
 
-// P - 2 (integer, string)
-// R - 1 String
-// E - Example
-// P - Pseudocode
+# n (integer): The number of times the string s should be repeated.
+# s (string): The string to be repeated.
+
+Return: The function will return a string that is the repetition of s exactly n times.
+
+Example:
+
+repeatString(3, "Hello"); // Output: "HelloHelloHello"
+repeatString(5, "OpenAI"); // Output: "OpenAIOpenAIOpenAIOpenAIOpenAI"
+repeatString(1, "JavaScript"); // Output: "JavaScript"
+
+PseudoCode:
+
+Initialize an empty string    
+Repeat the string 's' exactly 'n' times
+Return the repeated string
+
+*/
 
 // 1
 function repeatStr(n, s) {
   return s.repeat(n);
+}
+
+// 2
+
+function repeatStr(n, s) {
+  let result = '';
+  for (let i = 0; i < n; i++) {
+    result += s;
+  }
+  return result;
 }
